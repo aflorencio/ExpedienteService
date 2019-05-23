a564740b101759d99d7d6f37e87d49cfe2574a96
Alvaro <alvaro@eliminalia.com>
2019-05-23 10:04:50 +0200

    Proyecto con referencias.

---
+++ ExpedienteService/ExpedienteService/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpedienteService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
