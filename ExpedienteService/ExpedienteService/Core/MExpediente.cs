f0a7b7503bc33e5417df14c23efc4d22aca11a24
Alvaro <alvaro@eliminalia.com>
2019-05-24 11:26:15 +0200

    Implementacion completa de Expediente.

---
+++ ExpedienteService/ExpedienteService/Core/MExpediente.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpedienteService
+{
+    [BsonIgnoreExtraElements]
+    class MExpediente
+    {
+        [BsonId]
+        public ObjectId _id { get; set; }
+        [BsonIgnoreIfNull]
+        public List<ObjectId> contactoServiceID { get; set; }
+        [BsonIgnoreIfNull]
+        public List<ObjectId> rastreoServiceID { get; set; }
+        [BsonIgnoreIfNull]
+        public List<ObjectId> presupuestoServiceID { get; set; }
+        [BsonIgnoreIfNull]
+        public List<ObjectId> documentoServiceID { get; set; }
+        [BsonIgnoreIfNull]
+        public List<ObjectId> ticketServiceID { get; set; }
+        //Falta gestores y trabajadores que estan encargados del expediente
+
+        public DateTime fechaCreacion { get; set; }
+        public DateTime fechaUpdate { get; set; } //Cada vez que se haga un cambio en el expediente cambiara.
+        public int status { get; set; } // 0 descartado (motivo en ticket service deberia estar) 1 Pendiente por contacto 2 Pendiente de Rastreo 3 pendiente de presupuesto 4 pendiente de contrato 5 pendiente gestion 6 pendiente pago 7 otra gestion personalizada
+        public string statusInfo { get; set; } // 5/150 Enlaces borrados y separar despues. 
+
+    }
+}
--- ExpedienteService/ExpedienteService/Program.cs
+++ ExpedienteService/ExpedienteService/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Grapevine.Server;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,59 @@ namespace ExpedienteService
     {
         static void Main(string[] args)
         {
+
+            {
+                var v = "v0.1.0.0";
+                var nameService = "Expediente Service";
+                Console.Title = nameService + " " + v;
+                Console.WriteLine("     E X P E D I E N T E   S E R V I C E    " + v);
+                var serverStandar = new RestServer();
+
+                var input = "";
+                while ((input = Console.ReadLine()) != "q")
+                {
+                    switch (input)
+                    {
+                        case "start":
+                            Console.WriteLine("Starting service...");
+                            serverStandar.Port = "5008";
+                            serverStandar.Host = "*";
+                            serverStandar.Start();
+                            Console.Title = "[ON]  " + nameService + " " + v;
+
+                            break;
+
+
+                        case "start --log":
+                            Console.WriteLine("Starting service...");
+
+                            using (var server = new RestServer())
+                            {
+                                server.Port = "5008";
+                                server.Host = "*";
+                                server.LogToConsole().Start();
+                                Console.Title = "[ON]  " + nameService + " " + v;
+                                Console.ReadLine();
+                                server.Stop();
+                            }
+
+                            break;
+                        case "stop":
+                            Console.WriteLine("Stopping service...");
+                            serverStandar.Stop();
+                            Console.Title = nameService + " " + v;
+                            break;
+                        case "--version":
+                            Console.WriteLine(v);
+
+                            break;
+                        default:
+                            Console.WriteLine(String.Format("Unknown command: {0}", input));
+                            break;
+                    }
+
+                }
+            }
         }
     }
 }
