50fe87a9c73faaaf5882ba675f29695712778171
we <e>
2019-11-21 12:33:53 +0100

    Recursos

--- ExpedienteService/ExpedienteService/Core/MExpediente.cs
+++ ExpedienteService/ExpedienteService/Core/MExpediente.cs
@@ -25,6 +25,8 @@ namespace ExpedienteService
         public List<ObjectId> ticketServiceID { get; set; }
         //Falta gestores y trabajadores que estan encargados del expediente
 
+        // CONTRATO 
+
         public DateTime fechaCreacion { get; set; }
         public DateTime fechaUpdate { get; set; } //Cada vez que se haga un cambio en el expediente cambiara.
         public int status { get; set; } // 0 descartado (motivo en ticket service deberia estar) 1 Pendiente por contacto 2 Pendiente de Rastreo 3 pendiente de presupuesto 4 pendiente de contrato 5 pendiente gestion 6 pendiente pago 7 otra gestion personalizada
--- ExpedienteService/ExpedienteService/Core/MainCore.cs
+++ ExpedienteService/ExpedienteService/Core/MainCore.cs
@@ -1,12 +1,82 @@
-﻿using System;
+﻿using Mongo.CRUD;
+using MongoDB.Bson;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+using DMModel = ExpedienteService.MExpediente;
+
 namespace ExpedienteService
 {
     class MainCore
     {
+        public IMongoCRUD<DMModel> db;
+        public MainCore(string server, string database)
+        {
+            db = new MongoCRUD<DMModel>(server, database);
+        }
+
+        #region CREATE
+
+        public void Create(DMModel data)
+        {
+            try
+            {
+                db.Create(data);
+            }
+            catch (Exception e)
+            {
+            }
+        }
+
+        #endregion
+
+        #region READ
+        public List<DMModel> ReadAll()
+        {
+            BsonDocument filter = new BsonDocument();
+            filter.Add("_id", new BsonDocument()
+                    .Add("$exists", new BsonBoolean(true))
+            );
+            Mongo.CRUD.Models.SearchOptions hola = new Mongo.CRUD.Models.SearchOptions();
+
+            hola.PageSize = 10;
+            hola.PageNumber = 1;
+            var data = db.Search(filter, hola).Documents.ToList();
+
+            return data;
+
+        }
+        // FALTA ES BUSCAR POR CAMPO? ESTA EN QUERY       
+
+        public DMModel ReadId(string id)
+        {
+            return db.Get(ObjectId.Parse(id));
+        }
+
+        //public List<DMModel> ReadValue(string fieldName, string fieldValue)
+        //{
+
+
+        //}
+        #endregion
+
+        #region UPDATE
+        public void Update(string id, DMModel data)
+        {
+            DMModel document = new DMModel();
+
+            document = db.Get(ObjectId.Parse(id));
+            document = data;
+
+            db.Update(document); //SEGURO QUE TIENES QUE ENVIAR EL DATA O EL DOCUMENT. SI ES ASI ME CAMBIA LA ID FUNCIONA ESTO BIEN?
+
+        }
+
+        #endregion
+
     }
 }
